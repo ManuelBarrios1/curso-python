0f1b105cc82438dc54b20f2c811e0f656998f06c
Jose Manuel <19309010@utfv.edu.mx>
2022-06-07 22:07:47 -0500

    se agrega el proyecto

---
+++ Holamundo.py
@@ -0,0 +1,4 @@
+# Este programa te pide el nombre y te saluda.
+nombre = input ("¿Cómo te llamas?")
+
+print("Hola " + nombre)
---
+++ Tiposdevariables.py
@@ -0,0 +1,34 @@
+# String - Cadena de texto
+nombre = "Jose 😜"
+
+# Enteros - Numeros sin punto decimal
+
+edad = 19 # Operaciones matematicas
+
+edads = "19" # No se modifica el valor
+
+# ejemplo de diferencias
+print (edad + edad)
+print (edads + edads)
+
+# Float o Flotante - Número CON punto decimal 
+
+pi = 3.1416 
+
+edad = float(edad) # cesteo -  Transformar  un tipo de variable a otro
+
+#El float ocupa un espacio más grande
+
+print (edad)
+
+# Usa type () para saber de que tipo es la variable.
+print(type(nombre), type(edad))
+
+# Bool - Booleano - SI O NO
+
+tegusto = False 
+
+legustas = True 
+
+print(legustas)
+
